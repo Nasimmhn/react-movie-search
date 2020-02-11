b6a002686071e03ac640dc7912a9d66544a1a988
nasimmhn <nasim.mahzoun@gmail.com>
2020-02-11 22:27:11 +0100

    search bar styling

--- src/components/ShowList.js
+++ src/components/ShowList.js
@@ -46,59 +46,60 @@ export const ShowList = () => {
       <header>
         <nav>
           <div className="search-box">
-            <div>
-              <label htmlFor="search">Title:
-                <input name="search" type="text" defaultValue={title} />
+            <div className="title-search-wrapper">
+              <label htmlFor="search">
+                <input placeholder="Titles" name="search" type="text" defaultValue={title} />
               </label>
               <input type="submit" value="Search" onClick={(e) => { setTitle(e.target.previousSibling.children[0].value); setPage(1) }} />
             </div>
-            <div>
-              <label htmlFor="year">Year:
+            <div className="dropdowns-container">
+              <div>
+                <label htmlFor="year">Year:
                 <select name="year" onChange={(e) => { setYear(e.target.value); setPage(1) }} value={year}>
-                  <option defaultValue value="">ALL</option>
-                  <option value="2020">2020</option>
-                  <option value="2019">2019</option>
-                  <option value="2018">2018</option>
-                  <option value="2017">2017</option>
-                  <option value="2016">2016</option>
-                  <option value="2015">2015</option>
-                  <option value="2014">2014</option>
-                  <option value="2013">2013</option>
-                  <option value="2012">2012</option>
-                  <option value="2011">2011</option>
-                  <option value="2010">2010</option>
-                  <option value="2009">2009</option>
-                  <option value="2009">2008</option>
-                </select>
-              </label>
-            </div>
+                    <option defaultValue value="">ALL</option>
+                    <option value="2020">2020</option>
+                    <option value="2019">2019</option>
+                    <option value="2018">2018</option>
+                    <option value="2017">2017</option>
+                    <option value="2016">2016</option>
+                    <option value="2015">2015</option>
+                    <option value="2014">2014</option>
+                    <option value="2013">2013</option>
+                    <option value="2012">2012</option>
+                    <option value="2011">2011</option>
+                    <option value="2010">2010</option>
+                    <option value="2009">2009</option>
+                    <option value="2009">2008</option>
+                  </select>
+                </label>
+              </div>
 
-            <div>
-              <label htmlFor="genre">Genre:
+              <div>
+                <label htmlFor="genre">Genre:
                 <select name="genre" onChange={(e) => { setListed_in(e.target.value); setPage(1) }} value={listed_in}>
-                  <option defaultValue value="">ALL</option>
-                  <option value="action|adventure">Action & Adventure</option>
-                  <option value="comedies|stand-up">Comedy</option>
-                  <option value="dramas">Drama</option>
-                  <option value="docuseries|documentaries">Documentary</option>
-                  <option value="sci-fi|fantasy">Sci-Fi & Fantasy</option>
-                  <option value="thrillers">Thriller</option>
-                </select>
-              </label>
-            </div>
+                    <option defaultValue value="">ALL</option>
+                    <option value="action|adventure">Action & Adventure</option>
+                    <option value="comedies|stand-up">Comedy</option>
+                    <option value="dramas">Drama</option>
+                    <option value="docuseries|documentaries">Documentary</option>
+                    <option value="sci-fi|fantasy">Sci-Fi & Fantasy</option>
+                    <option value="thrillers">Thriller</option>
+                  </select>
+                </label>
+              </div>
 
-            <div>
-              <label htmlFor="perPage">results per page:
+              <div>
+                <label htmlFor="perPage">results per page:
                 <select name="perPage" onChange={(e) => { setPerPage(e.target.value); setPage(1) }} value={perPage}>
-                  <option value="100">100</option>
-                  <option defaultValue value="20">20</option>
-                  <option value="10">10</option>
-                  <option value="5">5</option>
-                </select>
-              </label>
+                    <option value="100">100</option>
+                    <option defaultValue value="20">20</option>
+                    <option value="10">10</option>
+                    <option value="5">5</option>
+                  </select>
+                </label>
+              </div>
+
             </div>
-
-
           </div>
         </nav>
       </header>
